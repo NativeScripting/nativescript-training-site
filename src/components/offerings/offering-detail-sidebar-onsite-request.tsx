5e615bf6f828e50811286e0bf8143b3bbe958b31
Alexander Ziskind <alex@nuvious.com>
2018-08-29 16:45:25 -0400

    trying out the netlify forms

--- src/components/offerings/offering-detail-sidebar-onsite-request.tsx
+++ src/components/offerings/offering-detail-sidebar-onsite-request.tsx
@@ -1,31 +1,100 @@
 import * as React from 'react';
+import { handleFormSubmit } from '../../util/form-utils';
 
 interface OfferingDetailSidebarOnsiteRequestProps {}
 
-export const OfferingDetailSidebarOnsiteRequestBox: React.StatelessComponent<
-  OfferingDetailSidebarOnsiteRequestProps
-> = (props: OfferingDetailSidebarOnsiteRequestProps) => {
-  return (
-    <div className="sidebar-widget sidebar-contact blog-details-content">
-      <h5>Onsite request form</h5>
+interface OfferingDetailSidebarOnsiteRequestState {
+  submitted: boolean;
+}
 
-      <div className="comment-form ">
-        <form action="#">
-          <div className="">
-            <div className="">
-              <input type="text" placeholder="Name" />
-              <input type="text" placeholder="Subject" />
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="">
-              <textarea placeholder="Message " />
-            </div>
+export class OfferingDetailSidebarOnsiteRequestBox extends React.Component<
+  OfferingDetailSidebarOnsiteRequestProps,
+  OfferingDetailSidebarOnsiteRequestState
+> {
+  constructor(props: OfferingDetailSidebarOnsiteRequestProps) {
+    super(props);
+
+    this.state = {
+      submitted: false,
+    };
+  }
+
+  public handleSubmit(event: React.FormEvent<EventTarget>) {
+    handleFormSubmit(event).then(() => {
+      this.setState({ submitted: true });
+    });
+  }
+
+  /*
+  public handleSubmit(event: React.FormEvent<EventTarget>) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const data = new FormData(form);
+
+    for (let name of (data as any).keys()) {
+      const input = form.elements[name];
+      const parserName = (input as any).dataset.parse;
+
+      if (parserName) {
+        const parser = inputParsers[parserName];
+        const parsedValue = parser(data.get(name));
+        data.set(name, parsedValue);
+      }
+    }
+
+    fetch(form.action, {
+      method: 'POST',
+      body: data,
+    }).then(() => {
+      this.setState({ submitted: true });
+    });
+  }
+  */
+
+  public render() {
+    return (
+      <div className="sidebar-widget sidebar-contact blog-details-content">
+        <h5>Onsite request form</h5>
+
+        {this.state.submitted && (
+          <div>Thank you. We will get back to you shortly.</div>
+        )}
+
+        {!this.state.submitted && (
+          <div className="comment-form ">
+            <form
+              name="private-request-sidebar"
+              onSubmit={e => this.handleSubmit(e)}
+              data-netlify="true"
+            >
+              <div className="">
+                <div className="">
+                  <input type="text" placeholder="Name" name="name" required />
+                  <input
+                    type="text"
+                    placeholder="Subject"
+                    name="subject"
+                    required
+                  />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="">
+                  <textarea placeholder="Message" name="message" required />
+                </div>
+                <div data-netlify-recaptcha />
+              </div>
+              <div>
+                <button className="theme-solid-button">Submit</button>
+              </div>
+            </form>
           </div>
-          <div>
-            <button className="theme-solid-button">Submit</button>
-          </div>
-        </form>
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
---
+++ src/util/form-utils.ts
@@ -0,0 +1,43 @@
+export const inputParsers: any = {
+  date(input: any) {
+    const [month, day, year] = input.split('/');
+    return `${year}-${month}-${day}`;
+  },
+  uppercase(input: any) {
+    return input.toUpperCase();
+  },
+  number(input: any) {
+    return parseFloat(input);
+  },
+};
+
+export function handleFormSubmit(
+  event: React.FormEvent<EventTarget>
+): Promise<Response> {
+  event.preventDefault();
+
+  const form = event.target as HTMLFormElement;
+  const data = new FormData(form);
+
+  for (let name of (data as any).keys()) {
+    const input = form.elements[name];
+    const parserName = (input as any).dataset.parse;
+
+    if (parserName) {
+      const parser = inputParsers[parserName];
+      const parsedValue = parser(data.get(name));
+      data.set(name, parsedValue);
+    }
+  }
+
+  //this is used by netlify
+  data.set('form-name', form.getAttribute('name'));
+
+  //submit data url encoded for netlify since they don't handle JSON body
+  const url = new URLSearchParams(data as any).toString();
+
+  return fetch(form.action, {
+    method: 'POST',
+    body: url,
+  });
+}
