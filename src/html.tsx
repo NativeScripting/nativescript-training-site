e8b1e61f69944df819d7d3c3b735ef16814b3f2a
Alexander Ziskind <alex@nuvious.com>
2018-09-07 09:10:19 -0400

    added favicon

--- src/html.tsx
+++ src/html.tsx
@@ -46,35 +46,38 @@ module.exports = React.createClass<HtmlProps, void>({
             name="viewport"
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
           />
+
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/img/favicon/apple-touch-icon.png"
+            href="/images/favicon/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/img/favicon/favicon-32x32.png"
+            href="/images/favicon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/img/favicon/favicon-16x16.png"
+            href="/images/favicon/favicon-16x16.png"
           />
-          <link rel="manifest" href="/img/favicon/manifest.json" />
+          <link rel="manifest" href="/images/favicon/site.webmanifest" />
           <link
             rel="mask-icon"
-            href="/img/favicon/safari-pinned-tab.svg"
+            href="/images/favicon/safari-pinned-tab.svg"
             color="#5bbad5"
           />
-          <link rel="shortcut icon" href="/img/favicon/favicon.ico" />
+          <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#603cba" />
           <meta
             name="msapplication-config"
-            content="/img/favicon/browserconfig.xml"
+            content="/images/favicon/browserconfig.xml"
           />
           <meta name="theme-color" content="#ffffff" />
+
           <link
             rel="image_src"
             href="https://nativescripting.com/img/course_banners/nativescript_core_ng_course_banner_bg.png"
